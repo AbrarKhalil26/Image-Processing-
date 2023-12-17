3e032d9535b84f1bd08caa23a03149eeb7f80092
hussein-salemm <husseinsalem177@gmail.com>
2023-12-17 20:03:37 +0200

    'Modified'

--- project2.m
+++ project2.m
@@ -84,76 +84,161 @@ global originImage
 grayImage = rgb2gray(originImage);
 noisyImage = grayImage;
 
+% Sobel Edge Detection
 if handles.popupmenu1.Value == 1
-    RGB = 0.2989*originImage(:,:,1) + 0.5870*originImage(:,:,2)+0.1140*originImage(:,:,3);
-    C = double(RGB);
-    for i=1:size(C, 1)-2
-        for j = 1:size(C, 2)-2
-           % x-direction
-           GX =((2*C(i,j+1)+C(i,j)+C(i,j+2))-(2*C(i+2,j+1)+C(i+2,j)+C(i+2,j+2)));
-           % y-direction
-           GY =((2*C(i+1,j+2)+C(i,j+2)+C(i+2,j+2))-(2*C(i+1,j)+C(i,j)+C(i+2,j)));
-            RGB(i,j) = sqrt(GX.^2 + GY.^2);
-        end
-    end
-    text = 'Sobel Edge Detection';
-    result = RGB;
+   result = manualSobelEdgeDetection(originImage);
+   text = 'Sobel Edge Detection';
     %------------------------------------------------------
+% Prewitt Edge Detection
 elseif handles.popupmenu1.Value == 2
-    prewittHorizontal = [-1 -1 -1; 0 0 0; 1 1 1];
-    prewittVertical = [-1 0 1; -1 0 1; -1 0 1];
-    gradientX = conv2(double(grayImage), prewittHorizontal, 'same');
-    gradientY = conv2(double(grayImage), prewittVertical, 'same');
-    gradientMagnitude = sqrt(gradientX.^2 + gradientY.^2);
-    normalizedGradient = uint8((gradientMagnitude / max(gradientMagnitude(:))) * 255);
-    
+    result = manualPrewittEdgeDetection(originImage);
     text = 'Prewitt Edge Detection';
-    result = normalizedGradient;
     %------------------------------------------------------
+% Add salt-and-papper noise
 elseif handles.popupmenu1.Value == 3
-    % Add salt-and-pepper noise manually
-    noiseDensity = 0.02;
-    % Generate random positions for salt noise
-    saltPositions = rand(size(noisyImage)) < noiseDensity / 2;
-    noisyImage(saltPositions) = 255; % Set salt noise to white (255)
-    % Generate random positions for pepper noise
-    pepperPositions = rand(size(noisyImage)) < noiseDensity / 2;
-    noisyImage(pepperPositions) = 0; % Set pepper noise to black (0)
-    noisyImage = uint8(noisyImage);
-    
+    result = addSaltAndPepperNoise(noisyImage, 0.02);
     text = 'Noisy Image';
-    result = noisyImage;
     %------------------------------------------------------
+% Denoised Image Using Median Filter *
 elseif handles.popupmenu1.Value == 4
     denoisedImage = medfilt2(noisyImage, [3, 3]);
     text = 'Denoised Image';
     result = denoisedImage;
     %------------------------------------------------------
+% Laplacian of Gaussian *
 elseif handles.popupmenu1.Value == 5
-    logFilter = fspecial('log', [5, 5], 0.5);
-    edges = imfilter(double(grayImage), logFilter, 'symmetric', 'conv');
-    % Normalize the result to the range [0, 255]
-    normalizedEdges = uint8((edges - min(edges(:))) * (255 / (max(edges(:)) - min(edges(:)))));
-
-    text = 'Laplacian of Gaussian';
-    result = normalizedEdges;
+   result = applyLaplacianOfGaussian(grayImage, [5, 5], 0.5);
+   text = 'Laplacian of Gaussian';
     %------------------------------------------------------
+% Histogram Equialization
 elseif handles.popupmenu1.Value == 6
-    equalizedImage = histeq(grayImage);
+    result = manualHistogramEqualization(grayImage);
     text = 'Histogram Equalization';
-    result = equalizedImage;
-    
+    %------------------------------------------------------
+% Canny Edge Detection
 elseif handles.popupmenu1.Value == 7
-    edgeImage = edge(grayImage, 'canny');
+    result = manualCannyEdgeDetection(grayImage);
     text = 'Canny Edge Detection';
-    result = edgeImage;
     
 end
 axes(handles.axes2);
 imshow(result)
 title(text);
 
+function result = manualSobelEdgeDetection(image)
+    % Sobel Edge Detection
+    RGB = 0.2989 * image(:,:,1) + 0.5870 * image(:,:,2) + 0.1140 * image(:,:,3);
+    C = double(RGB);
+    for i = 1:size(C, 1) - 2
+        for j = 1:size(C, 2) - 2
+            % x-direction
+            GX = ((2 * C(i, j + 1) + C(i, j) + C(i, j + 2)) - (2 * C(i + 2, j + 1) + C(i + 2, j) + C(i + 2, j + 2)));
+            % y-direction
+            GY = ((2 * C(i + 1, j + 2) + C(i, j + 2) + C(i + 2, j + 2)) - (2 * C(i + 1, j) + C(i, j) + C(i + 2, j)));
+            RGB(i, j) = sqrt(GX.^2 + GY.^2);
+        end
+    end
+    result = RGB;
+    
+function result = manualPrewittEdgeDetection(image)
+    % Convert image to grayscale
+    grayImage = rgb2gray(image);
 
+    % Prewitt Edge Detection
+    prewittHorizontal = [-1 -1 -1; 0 0 0; 1 1 1];
+    prewittVertical = [-1 0 1; -1 0 1; -1 0 1];
+    gradientX = conv2(double(grayImage), prewittHorizontal, 'same');
+    gradientY = conv2(double(grayImage), prewittVertical, 'same');
+    gradientMagnitude = sqrt(gradientX.^2 + gradientY.^2);
+    normalizedGradient = uint8((gradientMagnitude / max(gradientMagnitude(:))) * 255);
+
+    result = normalizedGradient;
+
+function result = addSaltAndPepperNoise(image, density)
+    % Add salt-and-pepper noise manually
+    noiseDensity = density;
+    
+    % Generate random positions for salt noise
+    saltPositions = rand(size(image)) < noiseDensity / 2;
+    image(saltPositions) = 255; % Set salt noise to white (255)
+    
+    % Generate random positions for pepper noise
+    pepperPositions = rand(size(image)) < noiseDensity / 2;
+    image(pepperPositions) = 0; % Set pepper noise to black (0)
+    
+    result = uint8(image);
+    
+function result = applyLaplacianOfGaussian(image, filterSize, sigma)
+    % Create Laplacian of Gaussian (LoG) filter
+    logFilter = fspecial('log', filterSize, sigma);
+    
+    % Apply LoG filter to the image
+    edges = imfilter(double(image), logFilter, 'symmetric', 'conv');
+    
+    % Normalize the result to the range [0, 255]
+    normalizedEdges = uint8((edges - min(edges(:))) * (255 / (max(edges(:)) - min(edges(:)))));
+    
+    result = normalizedEdges;
+    
+function result = manualHistogramEqualization(grayImage)
+    [counts, ~] = imhist(grayImage);
+    cdf = cumsum(counts) / sum(counts);
+    result = uint8(255 * cdf(grayImage + 1));
+
+function result = manualCannyEdgeDetection(grayImage)
+ % Gaussian smoothing
+    hsize = 5;
+    sigma = 1.4;
+    gaussianFilter = fspecial('gaussian', hsize, sigma);
+    smoothedImage = imfilter(double(grayImage), gaussianFilter, 'symmetric');
+
+    % Gradient calculation (Sobel operators)
+    sobelFilterX = [-1 0 1; -2 0 2; -1 0 1];
+    sobelFilterY = sobelFilterX';
+
+    gradientX = conv2(smoothedImage, sobelFilterX, 'same');
+    gradientY = conv2(smoothedImage, sobelFilterY, 'same');
+
+    % Gradient magnitude and direction
+    gradientMagnitude = sqrt(gradientX.^2 + gradientY.^2);
+    gradientDirection = atan2d(gradientY, gradientX);
+
+    % Non-maximum suppression
+    edgeImage = zeros(size(grayImage));
+    for i = 2:size(gradientMagnitude, 1) - 1
+        for j = 2:size(gradientMagnitude, 2) - 1
+            angle = gradientDirection(i, j);
+            if (angle >= -22.5 && angle < 22.5) || (angle >= 157.5 || angle < -157.5)
+                neighbor1 = gradientMagnitude(i, j + 1);
+                neighbor2 = gradientMagnitude(i, j - 1);
+            elseif (angle >= 22.5 && angle < 67.5) || (angle >= -157.5 && angle < -112.5)
+                neighbor1 = gradientMagnitude(i - 1, j + 1);
+                neighbor2 = gradientMagnitude(i + 1, j - 1);
+            elseif (angle >= 67.5 && angle < 112.5) || (angle >= -112.5 && angle < -67.5)
+                neighbor1 = gradientMagnitude(i - 1, j);
+                neighbor2 = gradientMagnitude(i + 1, j);
+            elseif (angle >= 112.5 && angle < 157.5) || (angle >= -67.5 && angle < -22.5)
+                neighbor1 = gradientMagnitude(i - 1, j - 1);
+                neighbor2 = gradientMagnitude(i + 1, j + 1);
+            end
+
+            if gradientMagnitude(i, j) >= neighbor1 && gradientMagnitude(i, j) >= neighbor2
+                edgeImage(i, j) = gradientMagnitude(i, j);
+            end
+        end
+    end
+
+    % Edge tracking by hysteresis
+    highThreshold = 0.2 * max(edgeImage(:));
+    lowThreshold = 0.1 * max(edgeImage(:));
+
+    strongEdges = edgeImage > highThreshold;
+    weakEdges = (edgeImage >= lowThreshold) & (edgeImage <= highThreshold);
+
+    % Use bwselect to connect weak edges to strong edges
+    edgeImage = edgeImage .* (strongEdges | bwmorph(weakEdges, 'bridge'));
+
+    result = uint8(edgeImage);
 
 % --- Executes on selection change in popupmenu1.
 function popupmenu1_Callback(hObject, eventdata, handles)
